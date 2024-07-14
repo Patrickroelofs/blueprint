7c191cfc3e7a71a7080c178e6d90cfd8394fed20
Patrick Roelofs <contact@patrickroelofs.com>
2024-07-14 13:33:52 +0200

    update settings to reflect nextjs requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,28 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(__dirname, "tsconfig.json");
+const project = resolve(__dirname, 'tsconfig.json');
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: [
-    require.resolve("@vercel/style-guide/eslint/browser"),
-    require.resolve("@vercel/style-guide/eslint/node"),
-    require.resolve("@vercel/style-guide/eslint/react"),
-    require.resolve("@vercel/style-guide/eslint/next"),
-    require.resolve("@vercel/style-guide/eslint/typescript"),
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
   ],
   parserOptions: { project },
-  settings: {},
-  overrides: [],
+  settings: {
+    'import/resolver': { typescript: { project } },
+  },
+  overrides: [
+    {
+      files: ['src/app/**/{page,layout}.tsx'],
+      rules: {
+        'import/no-default-export': 'off',
+        'import/prefer-default-export': ['error', { target: 'any' }],
+      },
+    },
+  ],
 };
