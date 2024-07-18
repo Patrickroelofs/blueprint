c8ca12119053b40e6dfc1bea0e868dfa7351a0b2
Patrick Roelofs <contact@patrickroelofs.com>
2024-07-18 17:05:47 +0200

    tst: intentionally make an eslint error to test action

--- src/lib/flow/node-types.ts
+++ src/lib/flow/node-types.ts
@@ -1,6 +1,7 @@
 import { TextNode } from '@/components/nodes/text-node';
-import { OutputNode } from '@/components/nodes/output-node';
 import { OpenAINode } from '@/components/nodes/openai-node';
+
+import { OutputNode } from '@/components/nodes/output-node';
 
 export const NodeTypes = {
   textNode: TextNode,
