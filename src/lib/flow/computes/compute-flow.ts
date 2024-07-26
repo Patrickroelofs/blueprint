3eb3f2f5671ce147d7b04eef3917f4f9f9dee4c3
Patrick Roelofs <contact@patrickroelofs.com>
2024-07-26 18:02:22 +0200

    cleanup

--- src/lib/flow/computes/compute-flow.ts
+++ src/lib/flow/computes/compute-flow.ts
@@ -1,8 +1,8 @@
 import { type Edge, type Node } from '@xyflow/react';
 
 function computeFlow(nodes: Node[], edges: Edge[]): void {
-  console.log(nodes, edges);
-  // TODO: Implement computeFlow functionality, this should automatically compute the flow of the nodes and edges
+  console.log('nodes:', nodes);
+  console.log('edges:', edges);
 }
 
 export { computeFlow };
